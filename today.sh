dd7fa03399af3059eead960eed4328bf898a7965
mike dupont <mike.dupont@introspector.local>
2024-03-01 09:49:41 -0500

    update

---
+++ today.sh
@@ -0,0 +1,14 @@
+
+today() {
+    TODAY=$HOME/`date +'%Y/%m/%d'`
+    if [ ! -d ${TODAY} ];
+    then
+        mkdir -p ${TODAY}
+    fi
+    cd ${TODAY}
+    echo $TODAY
+    pushd $TODAY
+}
+
+
+today
