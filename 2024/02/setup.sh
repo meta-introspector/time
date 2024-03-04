70428ec3c6775b30f57e9a7e70639973bfc93fdd
Ubuntu <ubuntu@t1v-n-cbea58e8-w-0.europe-west4-a.c.trc-tpu-project.internal>
2024-03-04 16:00:04 +0000

    setup

--- 2024/02/setup.sh
+++ 2024/02/setup.sh
@@ -1,3 +1,4 @@
+
 . "$HOME/.asdf/asdf.sh"
 
 asdf plugin-add erlang https://github.com/asdf-vm/asdf-erlang.git
@@ -9,3 +10,5 @@ asdf install elixir 1.12
 asdf global elixir 1.14.4-otp-26
 asdf install erlang 26.2.2
 asdf global erlang 26.2.2
+
+#export  LD_LIBRARY_PATH=/usr/local/lib/python3.10/dist-packages/libtpu
