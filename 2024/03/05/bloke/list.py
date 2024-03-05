e4a9158aa80fa2c50e994bcf341f3249f90e0125
mike dupont <mike.dupont@introspector.local>
2024-03-05 14:39:25 -0500

    first list

---
+++ 2024/03/05/bloke/list.py
@@ -0,0 +1,8 @@
+from huggingface_hub import HfApi, ModelFilter
+api = HfApi()
+models = api.list_models()
+models = list(models)
+print(len(models))
+for x in models:
+    print ("\t".join([x.id,str(x.downloads),str(x.likes),str(x)]))
+    
