b3fbfe22d376184fe1651bfe80285d65634d6ad8
mike dupont <mike.dupont@introspector.local>
2024-03-12 08:39:46 -0400

    guix

---
+++ 2024/03/12/guix.sh
@@ -0,0 +1,40 @@
+git submodule add https://git.savannah.gnu.org/git/guix.git
+
+#Documents and web site of bootstrappable.org
+git submodule add https://git.savannah.gnu.org/git/guix/bootstrappable.git
+
+#DHCP client in Guile
+git submodule add https://git.savannah.gnu.org/git/guix/dhcp.git
+
+#Guile bindings to GNUnet, and Guix substitutes over GNUnet file sharing
+git submodule add https://git.savannah.gnu.org/git/guix/gnunet.git
+
+#Artwork and web site of Guix
+git submodule add https://git.savannah.gnu.org/git/guix/guix-artwork.git
+
+#Cuirass continuous integration tool
+git submodule add https://git.savannah.gnu.org/git/guix/guix-cuirass.git
+
+#Articles, talks, and documents for Guix maintainers
+git submodule add https://git.savannah.gnu.org/git/guix/maintenance.git
+
+#Scripts for videos presenting Guix
+git submodule add https://git.savannah.gnu.org/git/guix/videos.git
+
+#Service to store and provide data about Guix
+git submodule add https://git.savannah.gnu.org/git/guix/data-service.git
+
+#Emacs interface to GNU Guix
+git submodule add https://git.savannah.gnu.org/git/guix/emacs-guix.git
+
+#Service for managing Guix builds
+git submodule add https://git.savannah.gnu.org/git/guix/build-coordinator.git
+
+#Manage a collection of Guix nar files
+git submodule add https://git.savannah.gnu.org/git/guix/nar-herder.git
+
+#Assist with Quality Assurance for Guix
+git submodule add https://git.savannah.gnu.org/git/guix/qa-frontpage.git
+
+#Mumi issue tracker
+git submodule add https://git.savannah.gnu.org/git/guix/mumi.git
