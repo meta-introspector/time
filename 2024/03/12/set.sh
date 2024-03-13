bc212e5b24e8dfe701bfd82261ec50a29a24c6d7
mike dupont <mike.dupont@introspector.local>
2024-03-13 15:56:27 -0400

    13th

---
+++ 2024/03/12/set.sh
@@ -0,0 +1,9 @@
+#/mnt/data1/nix/root/bin/nettle-hash
+RT=/mnt/data1/nix/root/
+export PKG_CONFIG_LIBDIR=$RT/lib
+export PKG_CONFIG_PATH=${RT}lib/pkgconfig:${RT}lib64/pkgconfig/
+#/mnt/data1/nix/root/lib64/pkgconfig/nettle.pc
+#/usr/local/lib/pkgconfig/libtasn1.pc
+export ACLOCAL_PATH=${RT}/share/aclocal/
+# sudo apt install libev-dev
+
