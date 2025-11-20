9090d03044d3f99741d507b6f0885d3b9434f57c
mike <h@solfunmeme.com>
2025-11-20 03:09:47 +0000

    wip packaging for cargo2nix

--- time/src/duration.rs
+++ time/src/duration.rs
@@ -10,6 +10,7 @@ use std::time::SystemTime;
 
 use deranged::RangedI32;
 use num_conv::prelude::*;
+use num_conv::Extend;
 
 use crate::convert::*;
 use crate::error;
@@ -1493,7 +1494,7 @@ macro_rules! duration_mul_div_int {
             fn mul(self, rhs: $type) -> Self::Output {
                 Self::nanoseconds_i128(
                     self.whole_nanoseconds()
-                        .checked_mul(rhs.cast_signed().extend::<i128>())
+                        .checked_mul(rhs as i128)
                         .expect("overflow when multiplying duration")
                 )
             }
@@ -1516,7 +1517,7 @@ macro_rules! duration_mul_div_int {
             #[track_caller]
             fn div(self, rhs: $type) -> Self::Output {
                 Self::nanoseconds_i128(
-                    self.whole_nanoseconds() / rhs.cast_signed().extend::<i128>()
+                    self.whole_nanoseconds() / rhs as i128
                 )
             }
         }
